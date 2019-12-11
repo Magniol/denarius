db7fedd6c010b9e0b2a8a7438e78027d1a26e437
Magniol <nmagniolalexandre@yahoo.com>
2019-12-11 02:24:06 -0500

    New commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,108 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="rgb(153, 51, 0"
+      dark
+    >
+      <div class="d-flex align-center">
+
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://scontent.fyhu1-1.fna.fbcdn.net/v/t1.15752-9/78571187_999310250401874_2848791403016749056_n.jpg?_nc_cat=103&_nc_ohc=-8ZXmHp0TeAAQmkMzPwJcotoSZHZEWqbYzdvxmjkzIpiivCWsupLmwvqA&_nc_ht=scontent.fyhu1-1.fna&oh=351eaff038ef1229a6dac1424903bc8b&oe=5E82C8D6"
+          transition="scale-transition"
+          width="150"
+        />
+      </div>
+
+      <v-spacer>
+      </v-spacer>
+
+      <div class="container">
+        <v-btn icon>
+          <h>About Us</h>
+        </v-btn>
+
+        <v-btn icon>
+          <h>What We Do</h>
+        </v-btn>
+
+        <v-btn icon>
+          <h>Ressources</h>
+        </v-btn>
+
+        <v-btn icon>
+          <h>History of Coin</h>
+        </v-btn>
+
+        <v-btn icon>
+          <h>Careers</h>
+        </v-btn>
+
+        <v-btn icon>
+          <h>Client Registration</h>
+        </v-btn>
+
+        <v-btn icon>
+          <h>About Us</h>
+        </v-btn>
+
+
+
+
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--      <v-btn-->
+<!--        href="https://github.com/vuetifyjs/vuetify/releases/latest"-->
+<!--        target="_blank"-->
+<!--        text-->
+<!--      >-->
+<!--        <span class="mr-2">Latest Release</span>-->
+<!--        <v-icon>mdi-open-in-new</v-icon>-->
+<!--      </v-btn>-->
+    </v-app-bar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
 </template>
 
+<script>
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  .container {
+    display: flex;
+    justify-content: space-between;
+   }
 </style>
+
