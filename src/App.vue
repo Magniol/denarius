6236a5ab4124efaf5a1063482f414c2380de52de
john <johnchafi@gmail.com>
2019-12-18 17:36:59 -0500

    footer

--- src/App.vue
+++ src/App.vue
@@ -293,9 +293,12 @@
                 'Web', 'Shopping', 'Videos', 'Images',
             ],
             more: [
-                'News', 'Maps', 'Books', 'Flights', 'Apps',
+                'News', 'Maps', 'Flights', 'Apps',
             ],
-            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            text: 'Lorem ipsum dolor sit amet, ' +
+                'consectetur adipiscing elit, sed do eiusmod tempor ' +
+                'incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ' +
+                'ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         }),
 
 
